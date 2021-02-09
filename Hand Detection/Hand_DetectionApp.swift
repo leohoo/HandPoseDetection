b3c1837f92df7e939119c9b3fba5c01b9c9b09c2
Wei Liu <leohoo@hotmail.com>
2021-02-09 11:35:55 +0900

    Initial Commit

---
+++ Hand Detection/Hand_DetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hand_DetectionApp.swift
+//  Hand Detection
+//
+//  Created by Wei Liu on 2021/02/09.
+//
+
+import SwiftUI
+
+@main
+struct Hand_DetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
